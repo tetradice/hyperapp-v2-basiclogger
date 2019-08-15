0455c809d22560e494eee143d06108edd8a2aabc
Dice <tetradice@gmail.com>
2019-08-16 01:57:26 +0900

    Separate files

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
 "use strict";
 
-export function defaultLogger(type, params){
-    switch(type){
-        case "action":
-            console.group("Dispatch action:", (params.action.name ? params.action.name : params.action));
-            if(params.payloadCreator !== undefined){
-                console.log("       default payload:", params.defaultPayload);
-                console.log("       payload creator:", params.payloadCreator);
-                console.log("created custom payload:", params.customPayload);
-
-            } else if(params.customPayload !== undefined){
-                console.log("default payload:", params.defaultPayload);
-                console.log(" custom payload:", params.customPayload);
-            } else {
-                console.log("default payload:", params.defaultPayload);
-                console.log(" custom payload:", "none");
-            }
-            console.groupEnd();
-
-            break;
-
-        case "state":
-            console.group("Dispatch state");
-            console.log("prev state:", params.prevState);
-            console.log("next state:", (params.changed ? '(unchanged)' : params.nextState));
-            console.groupEnd();
-            break;
-
-        case "stateAndEffects":
-            console.group("Dispatch state and Effects");
-            console.log("prev state:", params.prevState);
-            console.log("next state:", (params.changed ? params.nextState : "(unchanged)"));
-
-            var displayEffects = [];
-            for(var i = 0; i < params.effects.length; i++){
-                var effect = params.effects[i];
-                displayEffects.push({ runner: effect[0], props: effect[1] });
-            }
-
-            console.log("   effects:", displayEffects);
-            console.groupEnd();
-            break;
-    }
-}
+import { defaultLogger } from "./defaultLogger";
 
 export function createLoggerMiddleware(loggerFunction){
     return function (baseDispatch) {
