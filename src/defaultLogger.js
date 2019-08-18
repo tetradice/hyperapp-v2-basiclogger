45b9014c4716aef1a7ca0f7a3feba06cf94d20e4
Dice <tetradice@gmail.com>
2019-08-18 10:53:37 +0900

    Update log formatting (enable to display location of function and timestamp at invoking)

--- src/defaultLogger.js
+++ src/defaultLogger.js
@@ -1,21 +1,31 @@
 "use strict";
 
+function getFunctionName(func) {
+    return (func.name ? func.name : "anonymous")
+}
+
+function getTimestamp() {
+    return (new Date()).toLocaleString();
+}
+
 export function defaultLogger(type, params) {
     switch (type) {
         case "action":
-            console.group("%cDispatch action:", "color: gray; font-weight: lighter;", (params.action.name ? params.action.name : params.action));
-            if (params.payloadCreator !== undefined) {
-                console.log("%c       default payload:", "color: #03A9F4; font-weight: bold;", params.defaultPayload);
-                console.log("%c       payload creator:", "color: #9E9E9E; font-weight: bold;", params.payloadCreator);
-                console.log("%ccreated custom payload:", "color: #4CAF50; font-weight: bold;", params.customPayload);
-
-            } else if (params.customPayload !== undefined) {
-                console.log("%cdefault payload:", "color: #03A9F4; font-weight: bold;", params.defaultPayload);
-                console.log("%c custom payload:", "color: #4CAF50; font-weight: bold;", params.customPayload);
+            console.group("%cDispatch action: %c%s", "color: gray; font-weight: lighter;", "color: black;", getFunctionName(params.action));
+            if (params.customPayload !== undefined) {
+                console.log("%c     default payload: %o", "color: #03A9F4; font-weight: bold;", params.defaultPayload);
+                console.log("%c      custom payload: %o", "color: #4CAF50; font-weight: bold;", params.customPayload);
             } else {
-                console.log("%cdefault payload:", "color: #03A9F4; font-weight: bold;", params.defaultPayload);
-                console.log("%c custom payload: %cnone", "color: #4CAF50; font-weight: bold;", "color: gray;");
+                console.log("%c     default payload: %o", "color: #03A9F4; font-weight: bold;", params.defaultPayload);
+                console.log("%c      custom payload: %cnone", "color: #4CAF50; font-weight: bold;", "color: gray;");
             }
+            var detailOfFunctions = { action: params.action };
+            if (params.payloadCreator !== undefined) {
+                detailOfFunctions.payloadCreator = params.payloadCreator;
+            }
+            console.log("%c detail of functions: %o", "color: #9E9E9E; font-weight: bold;", detailOfFunctions);
+            console.log("%c                  at: %c%s", "color: #9E9E9E; font-weight: bold;", "color: #9E9E9E; font-weight: lighter;", getTimestamp());
+
             console.groupEnd();
 
             break;
@@ -30,7 +40,7 @@ export function defaultLogger(type, params) {
             if (params.changed) {
                 console.log("%cnew state: %cunchanged", "color: #4CAF50; font-weight: bold;", "color: gray;");
             } else {
-                console.log("%cnew state:", "color: #4CAF50; font-weight: bold;", params.newState);
+                console.log("%cnew state: %o", "color: #4CAF50; font-weight: bold;", params.newState);
             }
 
             if (params.effects !== undefined) {
@@ -40,7 +50,7 @@ export function defaultLogger(type, params) {
                     displayEffects.push({ runner: effect[0], props: effect[1] });
                 }
 
-                console.log("%c   effects:", "color: #03A9F4; font-weight: bold;", displayEffects);
+                console.log("%c  effects: %o", "color: #03A9F4; font-weight: bold;", displayEffects);
             }
 
             console.groupEnd();
